6e6325499a965836c8915433ebc2361f00556d72
BijianWu <384318185@qq.com>
2022-04-07 01:40:52 +0100

    to use the remote server domain set up in heroku

--- foreverg-frontend/src/apis/goals.js
+++ foreverg-frontend/src/apis/goals.js
@@ -1,5 +1,6 @@
 import axios from "axios";
+//local URL baseURL:"http://localhost:8000"
 
 export default axios.create({
-    baseURL:"http://localhost:8000"
+    baseURL:"https://forever-g-backend.herokuapp.com"
 })
